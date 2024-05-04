fa5a56ee5605c740172ded3227b7c341ba29f59b
yudai.usuda <yudai.usuda@zozo.com>
2024-05-04 21:24:10 +0900

    feat: add Checkbox

---
+++ src/Checkbox.test.ts
@@ -0,0 +1,23 @@
+import { Checkbox } from "./Checkbox";
+import { Status } from "./Status";
+
+describe("Checkbox", () => {
+  describe("fromLine", () => {
+    const testCase1 = "  - [x] Example task";
+    it(testCase1, () => {
+      const checkbox = Checkbox.fromLine(testCase1);
+
+      expect(checkbox.indentation).toBe("  ");
+      expect(checkbox.listMarker).toBe("-");
+      expect(checkbox.statusSymbol).toBe("x");
+      expect(checkbox.status).toEqual(Status.makeDone());
+      expect(checkbox.body).toBe("Example task");
+    });
+    const testCase2 = "This is not a valid task";
+    it(testCase2, () => {
+      expect(() => Checkbox.fromLine(testCase2)).toThrow(
+        "Line does not match task regex"
+      );
+    });
+  });
+});
