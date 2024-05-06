213b75f0846ffb50c1e33950c1ede7af52339f72
yudai.usuda <yudai.usuda@zozo.com>
2024-05-06 11:40:52 +0900

    fix: end is now matched only if there is a "-" in front of it
    To prevent 12:00 in "12:05 12:00-13:00 task" from being considered the end

--- src/Task.ts
+++ src/Task.ts
@@ -45,7 +45,7 @@ export class Task {
     /^/.source + // beginning of line
       /(?:(?<start>\d{1,2}:\d{1,2}))?/.source + // capture start time (HH:mm)
       /\s*-?\s*/.source + // separator(-)
-      /(?:(?<end>\d{1,2}:\d{1,2}))?/.source + // capture end time (HH:mm)
+      /(?:(?<end>(?<=\s*-\s*)\d{1,2}:\d{1,2}))?/.source + // capture end time (HH:mm)
       /\s*/.source + // whitespaces
       /(?<taskBody>.*)/.source //capture task body
   );
