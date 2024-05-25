931e47ed996fbeeb52c8f7583e2f7398c68094b5
yudai.usuda <yudai.usuda@zozo.com>
2024-05-25 17:40:49 +0900

    feat: add Duplicate as new task

--- src/commands.ts
+++ src/commands.ts
@@ -29,5 +29,30 @@ export function createCommands(settings: Settings): Command[] {
         editor.setCursor(line, ch);
       },
     },
+    {
+      id: "duplicate-as-new-task",
+      name: "Duplicate as new task",
+      icon: "copy-check",
+      editorCallback: (editor: Editor) => {
+        const { line, ch } = editor.getCursor();
+        const lineContent = editor.getLine(line);
+
+        const task = Task.fromLine(lineContent);
+
+        if (!task) {
+          new Notice("No task found");
+          return;
+        }
+
+        const taskOp = new taskOperations(settings);
+        const duplicated = taskOp.duplicateTask(task);
+
+        editor.replaceRange("\n" + duplicated.toString(), {
+          line,
+          ch: lineContent.length,
+        });
+        editor.setCursor(line, ch);
+      },
+    },
   ];
 }
